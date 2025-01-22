84d44846f27cfd3c543ba629ef01dae0cc350a49
Daniel Kruger <danielkruger98@gmail.com>
2025-01-22 14:27:40 +0200

    update friend_model
    add first and last name fields

--- lib/data/models/friend_model.dart
+++ lib/data/models/friend_model.dart
@@ -1,16 +1,22 @@
 class FriendModel {
   final String friendId;
   final String status;
+  final String firstName;
+  final String lastName;
 
   FriendModel({
     required this.friendId,
     required this.status,
+    required this.firstName,
+    required this.lastName,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'friendId': friendId,
       'status': status,
+      'first_name': firstName,
+      'last_name': lastName,
     };
   }
 
@@ -18,6 +24,8 @@ class FriendModel {
     return FriendModel(
       friendId: json['friendId'],
       status: json['status'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
     );
   }
 }
